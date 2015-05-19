32a2718af7cccbc9f012d607d30f0473ce9e6b03
de4dot <de4dot@gmail.com>
2015-05-19 19:00:50 +0200

    Add more text token types

--- ICSharpCode.NRefactory/TextTokenType.cs
+++ ICSharpCode.NRefactory/TextTokenType.cs
@@ -209,7 +209,77 @@ namespace ICSharpCode.NRefactory
 		/// </summary>
 		ILModule,
 
-		// If you add a new one, also update DnColorType
+		/// <summary>
+		/// ":" string
+		/// </summary>
+		XmlDocToolTipColon,
+
+		/// <summary>
+		/// "Example" string
+		/// </summary>
+		XmlDocToolTipExample,
+
+		/// <summary>
+		/// cref attribute in an exception tag
+		/// </summary>
+		XmlDocToolTipExceptionCref,
+
+		/// <summary>
+		/// "Returns" string
+		/// </summary>
+		XmlDocToolTipReturns,
+
+		/// <summary>
+		/// cref attribute in a see tag
+		/// </summary>
+		XmlDocToolTipSeeCref,
+
+		/// <summary>
+		/// langword attribute in a see tag
+		/// </summary>
+		XmlDocToolTipSeeLangword,
+
+		/// <summary>
+		/// "See also" string
+		/// </summary>
+		XmlDocToolTipSeeAlso,
+
+		/// <summary>
+		/// cref attribute in a seealso tag
+		/// </summary>
+		XmlDocToolTipSeeAlsoCref,
+
+		/// <summary>
+		/// name attribute in a paramref tag
+		/// </summary>
+		XmlDocToolTipParamRefName,
+
+		/// <summary>
+		/// name attribute in a param tag
+		/// </summary>
+		XmlDocToolTipParamName,
+
+		/// <summary>
+		/// name attribute in a typeparam tag
+		/// </summary>
+		XmlDocToolTipTypeParamName,
+
+		/// <summary>
+		/// "Value" string
+		/// </summary>
+		XmlDocToolTipValue,
+
+		/// <summary>
+		/// Summary text
+		/// </summary>
+		XmlDocSummary,
+
+		/// <summary>
+		/// Any other XML doc text
+		/// </summary>
+		XmlDocToolTipText,
+
+		// If you add a new one, also update ColorType
 
 		/// <summary>
 		/// Must be last
